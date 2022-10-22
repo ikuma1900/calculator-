20e4a10cdea512e79f56aac13803ecf7a7ca026d
ikuma1900 <107557059+ikuma1900@users.noreply.github.com>
2022-10-22 23:44:48 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,55 @@
-const buttons = document.querySelectorAll('button');
-const result = document.querySelector('#result');
-
-
-
-let conectText = "";
-
-function buttonPressed(event) {
-    const text = event.target.textContent;
-    
-    
-    if (text === "="){
-        conectText = eval(conectText);
-    }else  if (text === "C"){
-        conectText = "";
-    }else  {
-        conectText += text;
+function get_calc(btn) {
+    if(btn.value == "=") {
+        document.calculator.display.value = eval(document.calculator.display.value);
+    } else if (btn.value == "AC") {
+        document.calculator.display.value = "";
+    } else {
+        document.calculator.display.value += btn.value;
+        document.calculator.display.multi_btn.value = "*";
+        document.calculator.display.div_btn.value = "/";
     }
-    
-   
-    result.textContent = conectText;
 }
-buttons.forEach(button => button.addEventListener('click', buttonPressed));
+
+const operatorReg = /^(\+|-|\*|\/)$/;
+
+
+const numReg = /^([0-9]|00)$/;
+
+
+let canAddDecimalPoint = true;
+
+function get_calc(btn) {
+   
+   const preValue = document.calculator.display.value.slice(-1);
+   let value = btn.value;
+
+   if (value == ".") {
+
+      if (canAddDecimalPoint === false) return;
+      
+      if (preValue === "" || preValue === "." || operatorReg.test(preValue)) {
+         return;
+      }
+
+      canAddDecimalPoint = false;
+   }
+   else if (numReg.test(value) === false) {
+      canAddDecimalPoint = true;
+   }
+
+   if (operatorReg.test(value) && operatorReg.test(preValue)) {
+      return;
+   }
+
+   if (value == "00" && /^([0-9]|\.)$/.test(preValue) === false) {
+      value = "0"
+   }
+
+   if (value === "=") {
+      document.calculator.display.value = eval(document.calculator.display.value);
+   } else if (value === "AC") {
+      document.calculator.display.value = "";
+   } else {
+      document.calculator.display.value += value;
+   }
+}
