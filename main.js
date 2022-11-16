47abcd4dfcc54449cac4b4eb2deb0031f084db2f
ikuma1900 <107557059+ikuma1900@users.noreply.github.com>
2022-11-16 22:31:57 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,55 +1,30 @@
-function get_calc(btn) {
-    if(btn.value == "=") {
-        document.calculator.display.value = eval(document.calculator.display.value);
-    } else if (btn.value == "AC") {
-        document.calculator.display.value = "";
-    } else {
-        document.calculator.display.value += btn.value;
-        document.calculator.display.multi_btn.value = "*";
-        document.calculator.display.div_btn.value = "/";
-    }
+const result = document.getElementById("result")
+
+function edit(value) {
+if(result.value==='0' && value!=='.'){
+        result.value=value;
+    }else {result.value = result.value + value;}
 }
 
-const operatorReg = /^(\+|-|\*|\/)$/;
 
-
-const numReg = /^([0-9]|00)$/;
-
-
-let canAddDecimalPoint = true;
-
-function get_calc(btn) {
-   
-   const preValue = document.calculator.display.value.slice(-1);
-   let value = btn.value;
-
-   if (value == ".") {
-
-      if (canAddDecimalPoint === false) return;
-      
-      if (preValue === "" || preValue === "." || operatorReg.test(preValue)) {
-         return;
-      }
-
-      canAddDecimalPoint = false;
-   }
-   else if (numReg.test(value) === false) {
-      canAddDecimalPoint = true;
-   }
-
-   if (operatorReg.test(value) && operatorReg.test(preValue)) {
-      return;
-   }
-
-   if (value == "00" && /^([0-9]|\.)$/.test(preValue) === false) {
-      value = "0"
-   }
-
-   if (value === "=") {
-      document.calculator.display.value = eval(document.calculator.display.value);
-   } else if (value === "AC") {
-      document.calculator.display.value = "";
-   } else {
-      document.calculator.display.value += value;
-   }
+function calc() {
+    result.value = new Function(
+      "return " + result.value
+    )();
+  }
+function reset() {
+    result.value = "";
+  }  
+function operator(value){
+    if (result.value.slice(-1)==='+'){
+        return;
+    } else if (result.value.slice(-1)==='-'){
+        return;
+    } else if (result.value.slice(-1)==='*'){
+        return;
+    } else if (result.value.slice(-1)==='/'){
+        return;
+    } else if (result.value.slice(-1)==='.'){
+        return;
+    } else {result.value = result.value + value;}
 }
